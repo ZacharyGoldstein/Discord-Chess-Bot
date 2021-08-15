733406a903871cdac855a1bdb1852f2b160c42f2
dinosaue <88861320+dinosaue@users.noreply.github.com>
2021-08-14 19:13:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-import os 
+# import os 
 
 import discord
 import discord_slash
-import pandas as pd
 
-model_params = pd.read_csv("/Users/austinliu/Downloads/Discord-Chess-Bot-master/data/model_params_20210805.csv")
+import json 
+# import pandas as pd
+
+# model_params = pd.read_csv("/Users/austinliu/Downloads/Discord-Chess-Bot-master/data/model_params_20210805.csv")
 
 from discord.ext import commands
-from calcs import expected_date
-from calcs import get_predicted_date 
-from calcs import get_prob_success
-from calcs import get_predictor_values
-from calcs import process_rating_history
+# from calcs import expected_date
+# from calcs import get_predicted_date 
+# from calcs import get_prob_success
+# from calcs import get_predictor_values
+# from calcs import process_rating_history
 from calcs import score 
 
-from dotenv import load_dotenv
-load_dotenv()
+with open('/Users/austinliu/Downloads/Discord-Chess-Bot-master/secrets.json') as fin:
+  secrets = json.load(fin)
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# TOKEN = os.getenv('DISCORD_TOKEN')
 # GUILD = os.getenv('DISCORD_GUILD')
 
 # client = discord.Client()
@@ -36,20 +41,16 @@ async def whenrating(ctx, name, rating, variant="Rapid"):
   if not rating.isnumeric():
     await ctx.send("Error: Rating must be an integer.")
     return
-  (prob_success,predicted_date) = score(name,int(rating),variant,model_params)
-  print ("test1")
-  print ("prob success: ", prob_success)
-  print ("predicted date: ", predicted_date)
+  (prob_success,predicted_date) = score(name,int(rating),variant)
   await ctx.send(f"{name} has a {prob_success} chance of reaching a {variant} rating of {rating} within the next 2 years. If {name} succeeds, I predict the rating will be achieved around {predicted_date}.")  
 # except:
   if predicted_date == None:
     await ctx.send(f"Error: Not enough rating history for **{variant}** for **{name}**!")
-    print ("test")
     return
   elif predicted_date == -1:
     await ctx.send(f"Error: No lichess user with name **{name}**!")
     return
-  await ctx.send(f'{name} can expect to have a {variant} rating of {rating} on {date.strftime("%b %d, %Y")}.')
+  await ctx.send(f'{name} can expect to have a {variant} rating of {rating} on {predicted_date.strftime("%b %d, %Y")}.')
 
 
 # slash = discord_slash.SlashCommand(bot, sync_commands=True)
@@ -57,4 +58,6 @@ async def whenrating(ctx, name, rating, variant="Rapid"):
 # async def _whenrating(ctx, name, rating, variant="Rapid"):
 #   await whenrating(ctx, name, rating, variant)
 
-bot.run(TOKEN)
+# bot.run(TOKEN)
+
+bot.run(secrets.get('discord-token'))
